ff9cb6268690a3da2c06cafb8ef828d675b66a60
karolhas <hskdevprojects@gmail.com>
2024-09-03 11:32:12 +0200

    Fix: changed-form-page-design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import { Form } from "@/components/form/Form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Form />
+    <main className="flex min-h-screen items-center justify-center relative">
+      <div className="absolute left-0 top-0 h-full w-1/2 bg-red-400"></div>
+      <div className="absolute right-0 top-0 h-full w-1/2 bg-gray-100"></div>
+
+      <div className="relative z-10 bg-white p-8 shadow-xl rounded-lg w-full max-w-2xl m-4">
+        <Form />
+      </div>
     </main>
   );
 }
